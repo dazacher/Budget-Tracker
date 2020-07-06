ce31055c86809653c4213893399f60c407dcb183
Dazacher <dazacher2000@yahoo.com>
2020-07-06 07:29:35 -0700

    Added PWA functionality.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const compression = require("compression");
+// const compression = require("compression");
 
 const PORT = process.env.PORT || 3000;
 
@@ -9,11 +9,11 @@ const app = express();
 
 app.use(logger("dev"));
 
-app.use(compression());
+// app.use(compression());
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/budget",
   {
